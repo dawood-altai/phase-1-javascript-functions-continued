3e7d4ac41e5f9d09fbce0ba52791a75c619468f8
dawood <dawood.altai111@gmail.com>
2023-04-09 23:39:51 +0300

    Complated assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate")
+{
+    return `This Saturday, I want to ${activity}!`;
+  }
+  saturdayFun()
+  saturdayFun("bathe my dog")
+  
+  function mondayWork(task = "go to the office")
+   {
+    return `This Monday, I will ${task}.`;
+  }
+  mondayWork()
+  mondayWork("work from home")
+  
+  function wrapAdjective(flair = "*")
+  {
+    
+        return function(adjective = "special") {
+          return `You are ${flair}${adjective}${flair}!`;
+        }
+      
+      
+      const result = wrapAdjective("!!!");
+  }
